f55b002b9692c7d48626dca3780a77be6654bb00
Revajack <revathimaya1306@gmail.com>
2025-01-21 03:03:42 +0000

    file commit

--- docker.sh
+++ docker.sh
@@ -1,1 +1,5 @@
 docker build -t httpdimg .
+docker login -u revathijack -p Revajack@123456
+docker tag httpdimg:latest Revajack/httpdimg
+docker push Revajack/httpdimg
+docker run -itd --name con1 httpdimg
