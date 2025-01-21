5d0bcfe328a0764c6e4eac375c35a0359cd8aa5b
Revajack <revathimaya1306@gmail.com>
2025-01-21 19:13:32 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-docker build -t httpdimg .
+docker build -t httpdimg Dockerfile ./
 docker login -u revathijack -p Revajack@123456
 docker tag httpdimg:latest Revajack/httpdimg
 docker push Revajack/httpdimg
