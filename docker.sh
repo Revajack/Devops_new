6383ebabc0d8d10b250696c1ee62a6d1a437b01d
Revajack <revathimaya1306@gmail.com>
2025-01-22 14:05:57 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 docker build -t httpdimg:latest .
-docker login -u revathijack -p dckr_pat_8ZosCCradb8QAEEy84WGXQYTDBk
+docker login -u ${revathijack} -p ${dckr_pat_8ZosCCradb8QAEEy84WGXQYTDBk}
 docker tag httpdimg:latest Revathijack/httpdimg
 docker push Revathijack/httpdimg
 docker run -itd --name con1 httpdimg
